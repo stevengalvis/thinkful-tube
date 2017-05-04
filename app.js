120c21ee505d08a34801193fcd1b8c8f79697d21
stevengalvis <steven.galvis@gmail.com>
2017-05-04 07:29:24 -0400

    refactored getDataFromAPI

--- app.js
+++ app.js
@@ -1,45 +1,33 @@
 
 var YOU_TUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-function getDataFromAPI(searchTerm, callback){
+function getDataFromAPI(searchTerm){
     var query = {
         part: 'snippet',
         key: 'AIzaSyBHIpCEIVOD4TWR_x3XdfGuiB3Dkxxn_58',
         q: searchTerm
     }
-    $.getJSON(YOU_TUBE_URL, query, callback);
+    $.getJSON(YOU_TUBE_URL, query,function(data){
+        var resultElement = '';
+
+        if(data){
+            data.items.forEach(function(item){
+                resultElement += '<p>'+ '<img src ="' + item.snippet.thumbnails.medium.url +  '"</img>' +'</p>';
+            });
+        }
+        else {
+            resultElement += '<p>No results</p>';
+        }
+
+      $('.search-results').html(resultElement);
+    });
 }
-
-function displayYouTubeSearchData(data){
-    var resultElement = '';
-
-    if(data){
-        data.items.forEach(function(item){
-            resultElement += '<p>'+ '<img src ="' + item.snippet.thumbnails.medium.url +  '"</img>' +'</p>';
-        });
-    }
-    else {
-    resultElement += '<p>No results</p>';
-    }
-  $('.search-results').html(resultElement);
-}
-
-
-
-
-
-
-
-
-
 
 //event listeners
 function submit() {
     $('.search-form').on('submit', function(e) {
         e.preventDefault();
-        var query = $('.query').val();
-        getDataFromAPI(query,displayYouTubeSearchData);
-        console.log(query);
+        getDataFromAPI($('.query').val());
     });
 }
 $(function(){submit();});
